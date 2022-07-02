1a05b84d2b7c0b2b497e4750df9a5cc3e54ee7be
zaynpatel <65105835+zaynpatel@users.noreply.github.com>
2022-07-02 16:54:50 -0700

    Create ufo.cpp

---
+++ ufo.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include "ufo_functions.hpp"
+
+// for variables - data_type var_name
+
+int main() {
+
+  greet();
+
+  std::string codeword = "codecademy";
+  std::string answer = "__________";
+  int misses = 0;
+
+  std::vector<char> incorrect; 
+  bool guess = false;
+  char letter;
+
+  while (answer != codeword && misses < 7) {
+    
+    display_misses(misses);
+    display_status(incorrect, answer);
+
+    std::cout << "Please enter your guess: ";
+    std::cin >> letter;
+
+    for (int i = 0; i < codeword.length(); i++) {
+      if (letter == codeword[i]) {
+        answer[i] = letter;
+        guess = true;
+      }
+    }
+
+  if (guess) {
+ 
+  std::cout << "\nCorrect! You're closer to cracking the codeword.\n";
+ 
+} else {
+ 
+  std::cout << "\nIncorrect! The tractor beam pulls the person in further.\n";
+  incorrect.push_back(letter);
+  misses++;
+ 
+}
+
+    
+   
+  }
+
+end_game(answer, codeword); 
+}
